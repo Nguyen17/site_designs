aedcfe003c1ac22056f07714ca520e11574e99cc
Timmy Nguyen <message.timmynguyen@gmail.com>
2018-08-01 20:42:49 -0700

    modified menu

--- nike/src/components/SiteMenu.js
+++ nike/src/components/SiteMenu.js
@@ -1,56 +1,56 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class SiteMenu extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-      placeholder: "Search ..."
-    };
-  }
-  
-  render() {
-    return (
-      <div>
-      <div className="site-deals">
-          <p>Free Shipping on order over $25.</p>
-        </div>
-        <div className="site-menu">
-          {/* <div className="clear">
-    
+            placeholder: "Search ..."
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="site-deals">
+                    <p>Free Shipping on order over $25.</p>
+                </div>
+                <div className="site-menu">
+                    {/* <div className="clear">
+
           </div> */}
-          <div className="site-logo">
-          <img src="/assets/nikeLogo.svg" alt=""/>
-          <h3>NIKE</h3>
-          </div>
-  
-          <div className="site-options">
-            <ul>
-              <li>Login to Nike Account</li>
-              <li>Help</li>
-              <li>Shop</li>
-            </ul>
-          </div>
+                    <div className="site-logo">
+                        <img src="/assets/nikeLogo.svg" alt=""/>
+                        <h3>NIKE</h3>
+                    </div>
 
-          {/* Search Box */}
-          <input type="text" placeholder={this.state.placeholder}/>
+                    <div className="site-options">
+                        <ul>
+                            <li>Login to Nike Account</li>
+                            <li>Help</li>
+                            <li>Shop</li>
+                        </ul>
+                    </div>
 
-        </div>
+                    {/* Search Box */}
+                    <input type="text" placeholder={this.state.placeholder}/>
 
-        <div className="site-secondary-menu">
-          <ul>
-            <li>MEN</li>
-            <li>WOMEN</li>
-            <li>BOYS</li>
-            <li>GIRLS</li>
-            <li>BAGS</li>
-          </ul>
-        </div>
-        <div className="shipping-info">
-          <h2>$5 TWO DAY SHIPPING NOW AVAILABLE</h2>
-        </div>
-      </div>
-    )
-  }
+                </div>
+
+                <div className="site-secondary-menu">
+                    <ul>
+                        <li>MEN</li>
+                        <li>WOMEN</li>
+                        <li>BOYS</li>
+                        <li>GIRLS</li>
+                        <li>BAGS</li>
+                    </ul>
+                </div>
+                <div className="shipping-info">
+                    <h2>$5 TWO DAY SHIPPING NOW AVAILABLE</h2>
+                </div>
+            </div>
+        )
+    }
 }
